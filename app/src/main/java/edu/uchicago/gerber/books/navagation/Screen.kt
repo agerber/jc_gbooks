3ffd719d0e29196dd64b6e4b48ddca0279ac8796
Adam Gerber <adam.gerber@yahoo.com>
2022-07-11 23:56:45 -0500

    refactor Screen.kt

--- app/src/main/java/edu/uchicago/gerber/books/navagation/Screen.kt
+++ app/src/main/java/edu/uchicago/gerber/books/navagation/Screen.kt
@@ -3,7 +3,7 @@ package edu.uchicago.gerber.books.navagation
 import edu.uchicago.gerber.favs.R
 
 
-sealed class Screen(var route: String, var icon: Int, var title: String) {
-    object Search : Screen("search", R.drawable.ic_search, "Search")
-    object Detail : Screen("detail", 0, "Detail")
+sealed class Screen(var route: String) {
+    object Search : Screen("search")
+    object Detail : Screen("detail")
 }
